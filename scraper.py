4fcf08e3d35a8f33e50e1b21edbd7cecf344d4c2
Paul Bradshaw <paulonhismobile@gmail.com>
2018-01-25 11:17:17 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,8 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
+# import scraperwiki library to store data
+import scraperwiki
 # import lxml.html
 #
 # # Read in a page
